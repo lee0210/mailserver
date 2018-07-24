6cfcce6f5911195cc8b7d930d112fc0b6fac5792
Chunwai Lee <lee0210@outlook.com>
2018-07-24 14:20:15 +0000

    use secret to store custom information

--- bin/entrypoint.sh
+++ bin/entrypoint.sh
@@ -1,21 +1,15 @@
 #!/bin/sh
 
-postmap /etc/postfix/sasl_passwd
-chmod 600 /etc/postfix/sasl_passwd.db
+echo connect = host=db dbname=mail user=root password=$(cat /run/secrets/mysql_password) >> /etc/dovecot/dovecot-sql.conf.ext
+echo password = $(cat /run/secrets/mysql_password) >> /etc/postfix/mysql_alias_maps
+echo password = $(cat /run/secrets/mysql_password) >> /etc/postfix/mysql_mailbox_maps
+echo mydomain = $(cat /run/secrets/mydomain) >> /etc/postfix/main.cf
+
+postmap /run/secrets/sasl_passwd
 
 adduser -u 2000 mailuser
 addgroup -g 5000 mailgroup
 addgroup mailuser mailgroup
 chown -R mailuser:mailgroup /var/mail
-
-echo connect = host=db dbname=mail user=root password=$(cat /run/secrets/mysql_password) >> /etc/dovecot/dovecot-sql.conf.ext
-echo ssl_cert = \</etc/certs/$(cat /run/secrets/ca_name) >> /etc/dovecot/conf.d/10-ssl.conf
-echo ssl_key = \</etc/certs/$(cat /run/secrets/ca_name) >> /etc/dovecot/conf.d/10-ssl.conf
-
-echo password = $(cat /run/secrets/mysql_password) >> /etc/postfix/mysql_alias_maps
-echo password = $(cat /run/secrets/mysql_password) >> /etc/postfix/mysql_mailbox_maps
-echo smtpd_tls_cert_file = /etc/certs/$(cat /run/secrets/ca_name) >> /etc/postfix/main.cf
-echo smtpd_tls_key_file = /etc/certs/$(cat /run/secrets/ca_name) >> /etc/postfix/main.cf
-echo smtp_tls_CAfile = /etc/ssl/certs/$(cat /run/secrets/ca_name) >> /etc/postfix/main.cf
 
 exec runsvdir -P /etc/service
